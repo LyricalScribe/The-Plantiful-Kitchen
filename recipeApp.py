9c4a28ebf8b7418f0159cb5a514c27ba8e786a9b
Thulisile Ngomane <ThuleeN@outlook.com>
2023-09-19 23:08:27 +0200

    recipeApp.py

--- recipeApp.py
+++ recipeApp.py
@@ -39,18 +39,61 @@ class RecipeApp(object):
 
 
     def __run_search_query(self, query):
-        pass
+        
+        playsound(BUTTON_CLICK_SOUND)
+        query = self.search_entry.get()
+        recipe = self.__get_recipe(query)
+
+        if recipe:
+            recipe_image = recipe.image
+            recipe_url = recipe.url
+
+        else:
+            # Recipe not found
+            recipe_image = "https://www.mageworx.com/blog/wp-content/uploads/2012/06/Page-Not-Found-13.jpg"
+            recipe_url = ""
+
+        self.__show_image(recipe_image)
+        self.__get_ingredients(recipe)
+
+
+    def __get_recipe(self, query):
+        edamam_object = PyEdamam(recipes_appid=self.recipe_app_id, recipes_appkey=self.recipe_app_key)
+        query_result = edamam_object.search_recipe(query)
+        
+        for recipe in query_result:
+            return recipe
+
 
     def __show_image(self, image_url):
-        pass
+        response = requests.get(image_url)
+        
+        img = Image.open(BytesIO(response.content))
+        img = img.resize((RECIPE_IMAGE_WIDTH, RECIPE_IMAGE_HEIGHT))
+        image = ImageTk.PhotoImage(img)
+        
+        holder = tk.Label(self.window, image = image)
+        holder.photo = image
+        holder.grid(column=1, row=6, pady=10)
+
 
     def __get_ingredients(self, recipe):
-        pass
+        ingredients = tk.Text(master = self.window, height = 15, width = 50, bg = "#ffdada")
+        ingredients.grid(column=1,row=4, pady = 10)
+        ingredients.delete("1.0", tk.END)  
+
+        if recipe == None :
+            ingredients.insert(tk.END, "No Recipe found for search criteria")
+            return
+
+        ingredients.insert(tk.END, "\n" + recipe.label + "\n")
+        for ingredient in recipe.ingredient_names:
+            ingredients.insert(tk.END, "\n- " + ingredient)
 
 
     def run_app(self):
         self.window.mainloop()
-        pass
+        return
 
 if __name__ = "__main__":
     APP_ID = ""
